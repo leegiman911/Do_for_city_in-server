15127019ea796f5873fa6bcc6cff61f53cf1a9bf
JinSeop <turn3361@gmail.com>
2020-08-14 21:15:31 +0900

    :heavy_plus_sign: 회원가입 라우팅 설계,404요청 미들웨어 설계
     - 회원가입 라우팅 설계 DB설계되면 연결할 것
     - 404응답요청 미들웨어 완성

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const app = express();
 const PORT = 5000;
 
-app.use("/", (req, res) => {
-  res.send("Welcome to Man's Club");
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/signup", (req, res) => {
+  //req.body에 정보 있음!
+  //회원 가입 정보 db로 보내주세요!
+
+  res.sendStatus(201);
 });
 
+
+//404코드 보내는 미들웨어
+app.use(function (req, res, next) {
+  res.sendStatus(404);
+});
+
+
 app.listen(PORT, () => {
-  console.log(`server on ${PORT}`)
+  console.log(`server on ${PORT}`);
 });
