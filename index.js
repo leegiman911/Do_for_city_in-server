0bb714df0862110bff01ebdf6f091d40890b00c1
gunwooko <kkw722@gmail.com>
2020-08-17 22:12:49 +0900

    :hand: :heavy_plus_sign: 게시글 정보 API(게시글 상세 페이지)

--- index.js
+++ index.js
@@ -122,6 +122,39 @@ app.get("/mypage", (req, res) => {
   }
 });
 
+// 게시글 정보 API (게시글 상세 페이지)
+app.get("/contentDetail", (req, res) => {
+  if (req.session.session_id) {
+    // 클라이언트 측에서 body에 title과 createdAt를 같이 담아서 GET요청을 보낼 것이다.
+    db.Contents.findAll({
+      where: {
+        title: req.body.title,
+        createdAt: req.body.createdAt,
+      },
+      attributes: ["title", "content", "createdAt"],
+      include: [
+        { model: db.Users, as: "contents", attributes: ["userId"] },
+        {
+          model: db.Comments,
+          as: "commentsContent",
+          attributes: ["comment", "createdAt"],
+          include: [
+            { model: db.Users, as: "comments", attributes: ["userId"] },
+          ],
+        },
+      ],
+    }).then((contentDetail) => {
+      if (contentDetail) {
+        res.status(200).send(contentDetail);
+      } else {
+        res.status(404).send("요청하신 정보가 없습니다.");
+      }
+    });
+  } else {
+    res.status(400).send("잘못 요청하셨습니다.");
+  }
+});
+
 // 404코드 처리는 이후에 진행합니다.
 //404코드 보내는 미들웨어
 // app.use(function (req, res, next) {
