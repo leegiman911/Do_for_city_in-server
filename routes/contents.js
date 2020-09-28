ce8383f4187b6f1dfcab34aff631e151dea1b052
gunwooko <kkw722@gmail.com>
2020-09-28 20:51:27 +0900

    추가: S3 이미지파일 업로드 기능 + 패키지 추가

--- routes/contents.js
+++ routes/contents.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { contentsController } = require("../controller");
+const { fileUploadMiddleware } = require("../middlewares/s3fileupload");
+const { fileDeleteMiddleware } = require("../middlewares/s3fileDelete");
 
 // 게시판 정보 요청
 router.get("/", contentsController.contentInfo.get);
 
 // 게시글 수정 요청
+// 사진도 수정?
 router.put("/update", contentsController.contentUpdate.put);
 
 // 게시글 검색 요청
@@ -16,6 +19,8 @@ router.get("/search", contentsController.contentSearch.get);
 router.put("/comment/update", contentsController.commentUpdate.put);
 
 // 게시글 작성 요청
+// s3에 사진 저장할 때 사용합니다.
+router.use("/post", fileUploadMiddleware);
 router.post("/post", contentsController.contentPost.post);
 
 module.exports = router;
