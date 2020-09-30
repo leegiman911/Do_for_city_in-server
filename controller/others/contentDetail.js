6f7da4084f0834c19d9bd96128d498e155feaafe
leegiman911 <giman979@gmail.com>
2020-09-30 10:32:17 +0900

    게시글 상세 페이지 보고있는 유저의 아이디 추가

--- controller/others/contentDetail.js
+++ controller/others/contentDetail.js
@@ -2,9 +2,14 @@ const db = require("../../models");
 
 module.exports = {
   // 게시글 정보 API (게시글 상세 페이지)
-  post: (req, res) => {
+  post: async (req, res) => {
     if (req.session.session_id) {
       // 클라이언트 측에서 body의 data에(axios get요청) title과 createdAt를 같이 담아서 POST요청을 보낼 것이다.
+      const userData = await db.Users.findOne({
+        where: {
+          id: req.session.session_id,
+        },
+      }).then()
       db.Contents.findAll({
         where: {
           title: req.body.title,
@@ -24,7 +29,7 @@ module.exports = {
         ],
       }).then((contentDetail) => {
         if (contentDetail) {
-          res.status(200).send(contentDetail);
+          res.status(200).send([userData.userId, contentDetail]);
         } else {
           res.status(404).send("요청하신 정보가 없습니다.");
         }
